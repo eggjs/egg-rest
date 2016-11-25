5d9f2596b749707dff59f001a8344f8ea4a68610
TZ <atian25@qq.com>
2016-11-25 14:20:05 +0800

    feat: development helper with restc

--- app/middleware/rest.js
+++ app/middleware/rest.js
@@ -1,10 +1,18 @@
 'use strict';
 
+const restc = require('restc');
+
 module.exports = options => {
+  const middleware = restc.koa();
   return function* rest(next) {
     if (this.url.indexOf(options.urlprefix) === 0) {
-      // Make sure all RESTful APIs always respond with json format
-      this.response.type = 'json';
+      // if request with ?debug at development, then respond debug panel
+      if (!options.production && this.query.hasOwnProperty('debug')) {
+        yield middleware.call(this, next);
+      } else {
+        // Make sure all RESTful APIs always respond with json format
+        this.response.type = 'json';
+      }
     }
     yield* next;
   };
